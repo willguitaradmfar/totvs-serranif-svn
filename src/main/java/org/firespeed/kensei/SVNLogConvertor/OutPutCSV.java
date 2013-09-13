8b719c33ab85a28c9b92ebf28ae2756f5aa0d9da
William Lima Pereira <william.pereira@totvs.com.br>
2013-09-13 17:21:35 -0300

    ajuste de Deteccao de Issue no comentario
    Signed-off-by: William Lima Pereira <william.pereira@totvs.com.br>

--- src/main/java/org/firespeed/kensei/SVNLogConvertor/OutPutCSV.java
+++
@@ -1,9 +0,0 @@
-package org.firespeed.kensei.SVNLogConvertor;
-
-public class OutPutCSV {
-	
-	public static final String formatLineHead = "user;issue;review;date_review;file/dir;LOC:I;LOC:D";
-	public static final String formatLine = "%s;%s;%s;%s;%s;%s;%s";
-		
-
-}
